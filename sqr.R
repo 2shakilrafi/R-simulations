ea4fc46a34c40835edaf939c360b70bc1a8a3cad
Shakil Rafi <sarafi@uark.edu>
2023-12-27 16:06:21 -0600

    Adding the Phi_k and Phi networks

--- sqr.R
+++ sqr.R
@@ -1,2 +1,10 @@
+source("affn.R")
 
-sqr <- f
+sqr <- function(q,ve) {
+  
+  delta = 2^(-2 / (q - 2)) * ve^(q / (q - 2))
+  alpha = (ve / 2)^(1/(q - 2))
+  
+  M <- ceiling(0.5*log2(1 / ve))
+  return(c(alpha, delta,M))
+}
